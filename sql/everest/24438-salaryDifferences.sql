2237ae86dad548c76b231d53392aec374eb0dd21
Will S <73315256+wstoffers@users.noreply.github.com>
2021-03-13 13:08:00 -0700

    Adding SQL 24438

---
+++ sql/everest/24438-salaryDifferences.sql
@@ -0,0 +1,15 @@
+--select * from db_employee;
+SELECT
+    MAX(CASE WHEN
+            dept.department = 'marketing'
+        THEN
+            employees.salary
+        END) -
+    MAX(CASE WHEN
+            dept.department = 'engineering'
+        THEN
+            employees.salary
+        END) AS "salaryDifference"
+FROM db_employee AS employees
+JOIN db_dept AS dept ON
+    employees.department_id = dept.id;
