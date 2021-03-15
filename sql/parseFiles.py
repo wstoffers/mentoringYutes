ab05ab608f2589fbd52aeb62a2964722414f7e88
Will S <73315256+wstoffers@users.noreply.github.com>
2021-03-15 01:44:30 -0600

    Adding changes: dataFrame ready for BQ

--- sql/parseFiles.py
+++ sql/parseFiles.py
@@ -3,6 +3,7 @@
 #imports:
 import sys
 import os
+from pathlib import Path
 import re
 import numpy as np
 import pandas as pd
@@ -21,6 +22,11 @@ def collect(directory):
     for path, directories, files in os.walk(directory):
         if os.path.split(path)[1] in holdsSql:
             keep[path] = [x for x in files if re.match('[0-9]+-.*sql$',x)]
+    #order each group by file modification time:
+    for group in keep:
+        keep[group] = sorted(keep[group],
+                             key=lambda k:Path(os.path.join(group,
+                                                            k)).stat().st_mtime)
     return keep
 
 def ceilingDivide(x):
@@ -37,13 +43,13 @@ def parse(keep):
                 row = [int(cipher().decreaseDifficulty(matched.group(1))),
                        matched.group(2),
                        3-ceilingDivide(12%len(os.path.split(path)[1])),
+                       None,
                        np.nan,
-                       np.nan,
-                       np.nan,
+                       None,
                        np.nan,
                        usageArray(os.path.join(path,sql))]
                 frame.append(row)
-    print(frame)
+    return frame
 
 def usageArray(filePath):
     '''Extracts all uppercase words, functions, and phrases'''
@@ -52,8 +58,23 @@ def usageArray(filePath):
         groups = re.findall('[A-Z ]+',cleaned)
     return [x.strip() for x in groups if x.strip()]
 
+def combine(wholeFrame):
+    whole = pd.DataFrame(wholeFrame,columns=[x.name for x in schema])
+    parent = os.path.dirname(os.environ.get('CIPHERPATH'))
+    path = os.path.join(parent,'sqlMetadata.csv')
+    partial = pd.read_csv(path,true_values=['t'],
+                               false_values=['f'],
+                               skipinitialspace=True)
+    whole = whole.set_index('id')
+    partial = partial.set_index('id')
+    whole.update(partial)
+    whole = whole.reset_index()
+    return whole
+
 #run:
 if __name__ == '__main__':
     home = os.path.abspath(os.path.dirname(sys.argv[0])) 
     sqls = collect(home)
-    parse(sqls)
+    frame = parse(sqls)
+    dataFrame = combine(frame)
+    #dataFrame is ready for bq?
