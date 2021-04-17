ca32d5566d3a582fc2c45d8bc384db35e26d33c3
Will S <73315256+wstoffers@users.noreply.github.com>
2021-04-17 16:15:05 -0600

    Adding 24176

---
+++ sql/everest/24176-topStatesByBusiness.sql
@@ -0,0 +1,12 @@
+--ignore their select * from yelp_business;
+SELECT
+    state,
+    COUNT(1) AS "fiveStars"
+FROM yelp_business
+WHERE
+    stars = 5
+GROUP BY
+    state
+ORDER BY
+    "fiveStars" DESC,
+    state ASC;
